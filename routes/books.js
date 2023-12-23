e1f4b49661ec6474edde966612fec594bbc88412
Timothy Rodriguez <tim.rdrgz2017@live.com>
2023-12-23 14:50:43 -0600

    self styles

--- routes/books.js
+++ routes/books.js
@@ -3,7 +3,8 @@
 const express = require("express")
 const router = express.Router()
 
-
+// bring in our controller
+const bookController = require("../controllers/books.js")
 // ****** ROUTES - INDUCESS
 
 
@@ -11,130 +12,42 @@ const router = express.Router()
 
 
 // *INDEX - GET render all of the books 
-router.get("/", async (req, res) => {
-    // find all of the books
-    let books = await req.model.Book.find({})
-    res.render("index.ejs", {
-        books: books.reverse()
-    })
-    // render all of the books to index.ejs
-})
+router.get("/", bookController.index)
 
 
 // *NEW - GET for the form to create a new book
-router.get("/new", (req, res) => {
-    res.render("new.ejs") // render the file
-})
+router.get("/new", bookController.newForm)
 
 
 
 // * DELETE 
-router.delete("/:id", async (req, res) => {
-   try {
-        // Find a book and then delete
-        let deletedBook = await req.model.Book.findByIdAndDelete(req.params.id)
-        console.log(deletedBook)
-        // redirect back to the index
-        res.redirect("/books")
-
-   } catch (error) {
-    res.status(500).send("Somehting went wrong when deleting")
-   }
-})
+router.delete("/:id", bookController.destroy)
 
 
 
 //* UPDATE
-router.put("/:id", async (req, res) =>{
-    try{// handle our checkbox
-        if(req.body.completed === "on")  {
-            req.body.completed = true
-        } else {
-            req.body.completed = false
-        }
-        // Then find by id and update with the req.body
-        // findByIdAndUpdate - id, data to update, options obj
-        let updatedBook = await req.model.Book.findByIdAndUpdate(
-            req.params.id, 
-            req.body,
-            {
-                new: true
-            }
-        )
-        // redirect to the show route with the updated book
-        res.redirect(`/books/${updatedBook._id}`)
-    } catch (error) {
-        res.send("something went wrong")
-    }
-})
+router.put("/:id", bookController.update) 
 
 
 
 
 // *CREATE - POST
-router.post("/", async (req, res) => {
-    try {
-        if(req.body.completed === "on") {
-            //if checked
-            req.body.completed = true
-        } else {
-            // if not checked
-            req.body.completed = false
-    
-        }
-        let newBook = await req.model.Book.create(req.body)
-        res.redirect("/books")
-    } catch (err) {
-        res.send(err)
-    }
-})
+router.post("/", bookController.create)
 
 
 
 //* EDIT 
-router.get("/edit/:id", async (req, res) => {
-    try {
-        //find the book and edit
-        let foundBook = await req.model.Book.findById(req.params.id)
-        res.render("edit.ejs", {
-            book: foundBook
-        })
-    } catch (error) {
-        res.send("hello from the errerrereerer")
-    }
-})
+router.get("/edit/:id", bookController.edit)
 
 
 
 // * SEED - GET
-router.get("/seed", async (req, res) => {
-
-    try {
-        // delete everything in the database
-        await req.model.Book.deleteMany({})
-        // Create data in the database
-        await req.model.Book.create(
-            req.model.seedData
-        )
-        // redirect back to the index
-        res.redirect("/books")
-    } catch (error) {
-        res.send("Somefing went wrong with your seeds")
-    }
-})
+router.get("/seed", bookController.seed)
 
 
 
 // *SHOW - GET rendering only one book
-router.get("/:id", async (req, res) => {
-    // find book by _id
-    let foundBook = await req.model.Book.findById(req.params.id) // this is the request params object
-    console.log(foundBook)
-    // render show.ejs with the found book
-    res.render("show.ejs", {
-        book: foundBook
-    })
-})
+router.get("/:id", bookController.show)
 
 
 module.exports = router
--- server.js
+++ server.js
@@ -26,9 +26,10 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use(morgan("dev"))
-app.use(express.urlencoded({extended: true}))
+app.use(morgan("dev")) // logging
+app.use(express.urlencoded({extended: true})) // body parser this is how we get access to req.body
 app.use(methodOveride("_method")) // key word to pass to method override lets us use DELETE PUT HTTP verbs
+app.use("/public", express.static("public")) // this will serve up our public directory the url prefix of /public.styles.css
 
 // ************* ROUTES & ROUTER
 
